984b652d3d516028824d77de4c4122d150d99511
Matthias <mroeckl@users.noreply.github.com>
2023-08-05 23:09:37 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# electricity-prices
-Retrieve electricity prices from ENTSO-E
+# Electricity Prices
+
+This project retrieves day-ahead electricity prices from ENTSO-E.
+
+## Usage
+
+```
+API_KEY=<YOUR-FREE-ENTSOE-API-KEY> npm start
+```
---
+++ bin/export-electricity-prices.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const QueryPublication = require("entsoe-api-client").QueryPublication;
+const Area = require("entsoe-api-client").Area;
+
+queryEntsoe(getConfig()).then(function (entsoeResponse) {
+  const priceDocument = entsoeResponse[0];
+  const timeSeries = priceDocument.timeseries;
+  const pricesPerHour = extractPricesPerHour(timeSeries);
+  print(pricesPerHour);
+});
+
+function getConfig() {
+  const config = new Object();
+  config.startDate = new Date("2023-01-01T00:00:00Z");
+  config.endDate = new Date(new Date().setHours(0, 0, 0, 0));
+  config.apiKey = process.env.API_KEY;
+  config.docTypeDayAheadPrices = "A44"; // A44 - Day Ahead Prices
+  config.domainGermany = Area("BZN|DE-LU"); // Germany
+  return config;
+}
+
+function queryEntsoe(config) {
+  return QueryPublication(config.apiKey, {
+    documentType: config.docTypeactGenerationPerType,
+    documentType: config.docTypeDayAheadPrices,
+    inDomain: config.domainGermany,
+    outDomain: config.domainGermany,
+    startDateTime: config.startDate,
+    endDateTime: config.endDate,
+  });
+}
+
+function extractPricesPerHour(priceSeries) {
+  const pricesPerHour = new Object();
+
+  priceSeries.forEach((priceElement) => {
+    const pricePoint = priceElement.periods[0].points;
+    pricePoint.forEach((pricePoint) => {
+      const startDate = pricePoint.startDate;
+      if (isFullHour(startDate) && !isNaN(pricePoint.price)) {
+        pricesPerHour[startDate] = { price: pricePoint.price };
+      }
+    });
+  });
+
+  return pricesPerHour;
+}
+
+function isFullHour(startDate) {
+  return startDate.getMinutes() === 0;
+}
+
+function print(pricesPerHour) {
+  const sortedDateTimes = Object.keys(pricesPerHour).sort((date1, date2) => new Date(date1) - new Date(date2));
+  sortedDateTimes.forEach((dateTime) => {
+    console.log("%s;%d", new Date(dateTime).toISOString(), pricesPerHour[dateTime].price);
+  });
+}
